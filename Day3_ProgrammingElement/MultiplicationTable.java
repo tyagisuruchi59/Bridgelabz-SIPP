b696444d24f94ffb03eb7010f8cfdcfa9ab0dc5f
tyagisuruchi59 <suruchityagi972@gmail.com>
2025-06-15 23:15:17 +0530

    up

--- Day3_ProgrammingElement/MultiplicationTable.java
+++ Day3_ProgrammingElement/MultiplicationTable.java
@@ -1,5 +1,25 @@
 package Day3_ProgrammingElement;
 
+import java.util.Scanner;
+
+/**
+ * Program to find the multiplication table of a number from 6 to 9.
+ */
 public class MultiplicationTable {
-    
+    /**
+     * Main method to execute the program.
+     * @param args Command-line arguments (not used)
+     */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        // User input for number
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+        // Print multiplication table from 6 to 9
+        for (int i = 6; i <= 9; i++) {
+            System.out.println(number + " * " + i + " = " + (number * i));
+        }
+        scanner.close();
+    }
 }
+
