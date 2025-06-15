2a1725996cf2a28481d4bcb9aa4b173075a79367
tyagisuruchi59 <suruchityagi972@gmail.com>
2025-06-15 23:13:58 +0530

    up

--- Day3_ProgrammingElement/LeapYearLogical.java
+++ Day3_ProgrammingElement/LeapYearLogical.java
@@ -1,5 +1,25 @@
 package Day3_ProgrammingElement;
 
+import java.util.Scanner;
+
+/**
+ * Program to check if a year is a Leap Year using logical operators.
+ */
 public class LeapYearLogical {
-    
+    /**
+     * Main method to execute the program.
+     * @param args Command-line arguments (not used)
+     */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a year (>= 1582): ");
+        int year = scanner.nextInt();
+        if (year >= 1582 && ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))) {
+            System.out.println(year + " is a Leap Year.");
+        } else {
+            System.out.println(year + " is Not a Leap Year.");
+        }
+        scanner.close();
+    }
 }
+
