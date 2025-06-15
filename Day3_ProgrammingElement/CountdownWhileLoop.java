a6d925a9e39bde18c7053bcd819cc1838f5f89cd
tyagisuruchi59 <suruchityagi972@gmail.com>
2025-06-15 23:08:13 +0530

    up

--- Day3_ProgrammingElement/CountdownWhileLoop.java
+++ Day3_ProgrammingElement/CountdownWhileLoop.java
@@ -1,5 +1,32 @@
 package Day3_ProgrammingElement;
 
+import java.util.Scanner;
+
+/**
+ * Program to count down from user input value to 1 using a while loop (rocket launch countdown).
+ */
 public class CountdownWhileLoop {
-    
+    /**
+     * Main method to execute the program.
+     * @param args Command-line arguments (not used)
+     */
+    public static void main(String[] args) {
+        // Create Scanner object for user input
+        Scanner scanner = new Scanner(System.in);
+        
+        // User input for counter
+        System.out.print("Enter the countdown start value: ");
+        int counter = scanner.nextInt();
+        
+        // Countdown using while loop
+        while (counter >= 1) {
+            System.out.println(counter); // Print current counter value
+            counter--;
+        }
+        System.out.println("Rocket Launched!");
+        
+        // Close the scanner
+        scanner.close();
+    }
 }
+
