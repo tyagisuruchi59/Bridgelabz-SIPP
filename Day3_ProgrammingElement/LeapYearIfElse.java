07397ebc601a664cda18e33a04c8067014232e7d
tyagisuruchi59 <suruchityagi972@gmail.com>
2025-06-15 23:13:29 +0530

    up

--- Day3_ProgrammingElement/LeapYearIfElse.java
+++ Day3_ProgrammingElement/LeapYearIfElse.java
@@ -1,5 +1,33 @@
 package Day3_ProgrammingElement;
 
+import java.util.Scanner;
+
+/**
+ * Program to check if a year is a Leap Year using multiple if else statements.
+ */
 public class LeapYearIfElse {
-    
+    /**
+     * Main method to execute the program.
+     * @param args Command-line arguments (not used)
+     */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a year (>= 1582): ");
+        int year = scanner.nextInt();
+        if (year >= 1582) {
+            if (year % 400 == 0) {
+                System.out.println(year + " is a Leap Year.");
+            } else if (year % 100 == 0) {
+                System.out.println(year + " is Not a Leap Year.");
+            } else if (year % 4 == 0) {
+                System.out.println(year + " is a Leap Year.");
+            } else {
+                System.out.println(year + " is Not a Leap Year.");
+            }
+        } else {
+            System.out.println("Year must be 1582 or later.");
+        }
+        scanner.close();
+    }
 }
+
